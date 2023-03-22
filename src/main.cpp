8f4bd4627bdb0aeed4d9ab2f8dd278c58abfda96
Henry Sjøen <henry.sjoen@miles.no>
2023-03-22 13:26:10 +0100

    autorefactor

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,33 @@
 #include <ArduinoJson.h>
 #include "secrets.h"
 
-// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
-// Adjust these values to match your project
-const int numButtons = 3;                                                        // Number of buttons
-const int buttonPins[numButtons] = {6, 7, 8};                                    // Array of button pins
-int ledPins[numButtons] = {1, 2, 3};                                             // Array of LED pins
-const String projects[numButtons] = {"KraftBank", "MilesDashboard", "Internal"}; // Array of project names
-// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+const int NUM_BUTTONS = 3;
+const int BUTTON_PINS[NUM_BUTTONS] = {6, 7, 8};
+int LED_PINS[NUM_BUTTONS] = {1, 2, 3};
+const String PROJECTS[NUM_BUTTONS] = {"KraftBank", "MilesDashboard", "Internal"};
 
-bool buttonState[numButtons] = {false};      // Array to keep track of button state
-bool projectIsRunning[numButtons] = {false}; // Array to keep track of project state
+bool buttonState[NUM_BUTTONS] = {false};
+bool projectIsRunning[NUM_BUTTONS] = {false};
 int status = WL_IDLE_STATUS;
 
 void setup()
 {
-  for (int i = 0; i < numButtons; i++)
-  {
-    pinMode(buttonPins[i], INPUT_PULLUP);
-    pinMode(ledPins[i], OUTPUT);
-  }
-
+  initializePins();
   Serial.begin(9600);
+  connectToWiFi();
+}
 
-  // attempt to connect to WiFi network
+void initializePins()
+{
+  for (int i = 0; i < NUM_BUTTONS; i++)
+  {
+    pinMode(BUTTON_PINS[i], INPUT_PULLUP);
+    pinMode(LED_PINS[i], OUTPUT);
+  }
+}
+
+void connectToWiFi()
+{
   while (status != WL_CONNECTED)
   {
     Serial.print("Attempting to connect to SSID: ");
@@ -42,15 +46,22 @@ void setup()
 void buttonPressed(int button)
 {
   Serial.println("Button " + String(button) + " pressed");
+  String url = constructApiUrl(button);
+  sendApiRequest(url);
+  toggleProjectState(button);
+}
 
-  // Construct URL for API call
+String constructApiUrl(int button)
+{
   String endpoint = projectIsRunning[button] ? "/stop" : "/start";
-  String url = String("http://") + String(API_HOST) + String(":") + String(API_PORT) + endpoint + "?project=" + String(projects[button]);
+  return String("http://") + String(API_HOST) + String(":") + String(API_PORT) + endpoint + "?project=" + String(PROJECTS[button]);
+}
 
+void sendApiRequest(String url)
+{
   Serial.println("API URL: " + url);
-
-  // Send HTTP request to API
   WiFiClient client;
+
   if (client.connect(API_HOST, API_PORT))
   {
     Serial.println("Connected to API server");
@@ -58,30 +69,34 @@ void buttonPressed(int button)
                  "Host: " + String(API_HOST) + "\r\n" +
                  "Connection: close\r\n" +
                  "\r\n");
+    readApiResponse(client);
   }
   else
   {
     Serial.println("Connection to API server failed");
   }
+}
 
-  // Read response from API
+void readApiResponse(WiFiClient &client)
+{
   while (client.connected())
   {
     String line = client.readStringUntil('\n');
     Serial.println(line);
   }
-
   client.stop();
+}
 
-  // Toggle projectIsRunning flag
+void toggleProjectState(int button)
+{
   projectIsRunning[button] = !projectIsRunning[button];
 }
 
 void readButtons()
 {
-  for (int i = 0; i < numButtons; i++)
+  for (int i = 0; i < NUM_BUTTONS; i++)
   {
-    bool currentState = digitalRead(buttonPins[i]) == LOW;
+    bool currentState = digitalRead(BUTTON_PINS[i]) == LOW;
     if (currentState && !buttonState[i])
     {
       buttonState[i] = true;
@@ -96,9 +111,9 @@ void readButtons()
 
 void updateLEDs()
 {
-  for (int i = 0; i < numButtons; i++)
+  for (int i = 0; i < NUM_BUTTONS; i++)
   {
-    digitalWrite(ledPins[i], projectIsRunning[i] ? HIGH : LOW);
+    digitalWrite(LED_PINS[i], projectIsRunning[i] ? HIGH : LOW);
   }
 }
 
@@ -112,24 +127,31 @@ void updateProjectStatus()
   lastUpdateTime = millis();
   Serial.println("Updating project status");
 
-  // Todo: Do not block the main loop
-  // Make GET request to get running projects
   String url = String("http://") + String(API_HOST) + String(":") + String(API_PORT) + "/running";
   WiFiClient client;
+
   if (client.connect(API_HOST, API_PORT))
   {
-    client.println("GET " + url + " HTTP/1.1");
-    client.println("Host: " + String(API_HOST));
-    client.println("Connection: close");
-    client.println();
+    sendStatusRequest(client, url);
+    String response = readStatusResponse(client);
+    updateProjectStatusFromResponse(response);
   }
   else
   {
     Serial.println("Connection failed");
-    return;
   }
+}
 
-  // Read response from server
+void sendStatusRequest(WiFiClient &client, String url)
+{
+  client.println("GET " + url + " HTTP/1.1");
+  client.println("Host: " + String(API_HOST));
+  client.println("Connection: close");
+  client.println();
+}
+
+String readStatusResponse(WiFiClient &client)
+{
   Serial.println("Reading response");
   String response = "";
   while (client.connected())
@@ -138,21 +160,20 @@ void updateProjectStatus()
     response += line;
   }
   client.stop();
+  return response;
+}
 
-  // Parse response and update project status
-  for (int i = 0; i < numButtons; i++)
+void updateProjectStatusFromResponse(String response)
+{
+  for (int i = 0; i < NUM_BUTTONS; i++)
   {
     bool isRunning = false;
-    for (int j = 0; j < response.length(); j++)
+    String projectName = "\"" + PROJECTS[i] + "\"";
+    if (response.indexOf(projectName) != -1)
     {
-      String projectName = "\"" + projects[i] + "\"";
-      if (response.indexOf(projectName) != -1)
-      {
-        isRunning = true;
-        break;
-      }
+      isRunning = true;
     }
-    Serial.println(projects[i] + " is running: " + isRunning);
+    Serial.println(PROJECTS[i] + " is running: " + isRunning);
     projectIsRunning[i] = isRunning;
   }
   Serial.println("Done updating project status");
@@ -161,7 +182,6 @@ void updateProjectStatus()
 bool needToUpdate = true;
 void loop()
 {
-  // wait for WiFi connection
   if (WiFi.status() != WL_CONNECTED)
   {
     needToUpdate = true;
@@ -171,6 +191,7 @@ void loop()
 
   readButtons();
   updateLEDs();
+
   if (needToUpdate)
   {
     updateProjectStatus();
